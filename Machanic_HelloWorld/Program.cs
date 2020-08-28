d7ad97a9b0b6a135932e00ca7cb9bf59c2df5f29
Tyler Machanic <trm2418@rit.edu>
2020-08-28 16:06:12 -0400

    creating HelloWorld

---
+++ Machanic_HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machanic_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tyler Machanic");
+            String word = "word";
+            Console.WriteLine(word);
+            String word2 = word;
+            int num2 = 2;
+            int num3 = num2;
+            num3 += 1;
+            Console.WriteLine(word);
+            while (num2 < 5)
+            {
+                Console.WriteLine(num2);
+                num2++;
+            }
+        }
+    }
+}
