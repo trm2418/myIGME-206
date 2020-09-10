d959236c3225f41863fac6ba17d8a7c4b40745de
Tyler Machanic <trm2418@rit.edu>
2020-09-09 20:39:52 -0400

    finished ReadLineDelegate

---
+++ ReadLineDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReadLineDelegate
+{
+    // Class: Program
+    // Author: Tyler Machanic
+    // Purpose: PE9 Question 3
+    // Restrictions: None
+    class Program
+    {
+        // define delegate function data type
+        delegate string readLineImpersonation();
+
+        // Method: Main
+        // Purpose: Declare and set delegate variable, ask for input and print
+        //          Output: User input
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // declare delegate variable
+            readLineImpersonation readLine;
+
+            // set delegate variable to Console.ReadLine function
+            readLine = new readLineImpersonation(Console.ReadLine);
+
+            // ask for input
+            Console.Write("Input: ");
+
+            // print output using delegate variable instead Console.ReadLine
+            Console.WriteLine(readLine());
+        }
+    }
+}
