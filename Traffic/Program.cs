4f3dfd2342ff45413fab59f52886c9afa79812d2
Tyler Machanic <trm2418@rit.edu>
2020-09-19 21:52:44 -0400

    finished Traffic

---
+++ Traffic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Vehicles;
+
+namespace Traffic
+{
+    // Class: Program
+    // Author: Tyler Machanic
+    // Purpose: PE11 Question 6
+    // Restrictions: None
+    class Program
+    {
+        // Method: AddPassenger
+        // Purpose: Calls LoadPassenger method from IPassengerCarrier
+        //          interface and prints the object using ToString()
+        // Restrictions: None
+        static void AddPassenger(IPassengerCarrier vehicleObject)
+        {
+            vehicleObject.LoadPassenger();
+            Console.WriteLine(vehicleObject.ToString());
+        }
+
+        // Method: Main
+        // Purpose: Create a passengerTrain object and call AddPassenger method
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            PassengerTrain passengerTrain = new PassengerTrain();
+            AddPassenger(passengerTrain);
+            //FreightTrain freightTrain = new FreightTrain();
+            //AddPassenger(freightTrain);
+        }
+    }
+}
