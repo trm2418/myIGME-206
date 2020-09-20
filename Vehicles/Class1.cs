70291066bcfce9110f05397a5140e74260c61199
Tyler Machanic <trm2418@rit.edu>
2020-09-19 20:54:06 -0400

    finished Vehicles

---
+++ Vehicles/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    public abstract class Car : Vehicle
+    {
+
+    }
+
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    public class Compact : Car, IPassengerCarrier
+    {
+
+    }
+
+    public class SUV : Car, IPassengerCarrier
+    {
+
+    }
+
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+
+    }
+
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+
+    }
+
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+
+    public class Class1
+    {
+    }
+}
---
+++ Vehicles/Vehicle.cs
@@ -0,0 +1,73 @@
+﻿// File: Vehicle.cs
+// Author: Tyler Machanic
+// Purpose: define Vehicle family of objects for PE11 Question 5
+// Restrictions: None
+
+namespace Vehicles
+{
+    // abstract vehicle class
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    // abstract car class
+    public abstract class Car : Vehicle
+    {
+
+    }
+
+    // passenger carrier interface
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    // heavy load carrier interface
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+
+    // abstract train class
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    // compact class
+    public class Compact : Car, IPassengerCarrier
+    {
+
+    }
+
+    // SUV class
+    public class SUV : Car, IPassengerCarrier
+    {
+
+    }
+
+    // pickup class
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+
+    }
+
+    // passenger train class
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+
+    }
+
+    // freight train class
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+
+    // 424 double bogey class
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+}
