f17df34b1b35089d7cec27773223c78f75aa8f10
Tyler Machanic <trm2418@rit.edu>
2020-12-08 23:40:14 -0500

    final project console version

---
+++ Final Project/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClassLibrary;
+
+namespace Final_Project
+{
+    class Program
+    {
+        // seed the Random
+        static Random rnd = new Random();
+
+        // Main class, creates the parties and calls the battle method
+        class Main2
+        {
+            
+
+            // Main method, creates parties and calls Battle method
+            public static void Main(string[] args)
+            {
+                GameHandler.Main(args);
+            }
+        }
+    }
+}
